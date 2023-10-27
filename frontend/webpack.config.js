313a609b4f9e9161f7d00f8b3298f922ddf61237
NimfaMargo <Nikiforova.Rita@gmail.com>
2023-10-27 18:01:31 +0300

    ad eslint

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,46 +1,46 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const config = require('./config/config');
+const config = require('./config/config'); 
 
 module.exports = {
-	entry: './src/index.tsx',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		})
-	],
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js', '.mjs']
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js'
-	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist'),
-		},
-		compress: true,
-		hot: true,
-		port: 3000,
-		proxy: {
-			'/api': {
-				target: config.baseURL,
-				changeOrigin: true,
-			}
-		}
-	},
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.mjs'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    hot: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: config.baseURL,
+        changeOrigin: true,
+      },
+    },
+  },
 };
